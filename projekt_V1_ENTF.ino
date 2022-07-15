617a2eacc3d4bab43061c4334d25b4ef137324bb
drouver-helmuth <51882074+drouver-helmuth@users.noreply.github.com>
2022-07-15 15:28:53 +0200

    added new branch version

--- projekt_V1_ENTF.ino
+++ projekt_V1_ENTF.ino
@@ -1,83 +1,108 @@
+
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(8,9,10,11,12,13);
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13); // LCD-Anschluesse (Ansachluss LCD - Pin Ardunino): RS-8;E-9;D4-10;D5-11;D6-12;D7-13
 
+long measure_distance(int trigger, int echo)
+{
+  long dauer, entfernung;
 
-int trigger1 = 2; 
-int echo1 = 3;
+  digitalWrite(trigger, LOW);
+  delay(5);
+  digitalWrite(trigger, HIGH);
+  delay(2);
+  digitalWrite(trigger, LOW);
+  dauer = pulseIn(echo, HIGH);
 
-int trigger2 = 5;
-int echo2 = 6;
+  entfernung = (dauer / 2) * 0.3432;
+  return entfernung;
+}
 
-long dauer1 = 0, dauer2 = 0;
-long entfernung1 = 0, entfernung2 = 0; 
+int trigger1 = 2; // Anschluss Trigger des Ultraschallsensors 1 auf Pin 2
+int echo1 = 3;    // Anschluss Echo des Ultraschallsensors 1 auf Pin 3
+
+int trigger2 = 5; // Anschluss Trigger des Ultraschallsensors 2 auf Pin 5
+int echo2 = 6;    // Anschluss Trigger des Ultraschallsensors 2 auf Pin 6
+
 void setup()
 {
-  Serial.begin (9600); 
-  
-  pinMode(trigger1, OUTPUT); 
-  pinMode(echo1, INPUT); 
-  
+  Serial.begin(9600);
+
+  pinMode(trigger1, OUTPUT);
+  pinMode(echo1, INPUT);
+
   pinMode(trigger2, OUTPUT);
   pinMode(echo2, INPUT);
 
   pinMode(4, OUTPUT);
   pinMode(7, OUTPUT);
-  
-  lcd.begin(16,2);
+
+  lcd.begin(16, 2);
   Serial.begin(9600);
-  lcd.setCursor(0,0);
-  lcd.print("Entfernung:");
 }
 void loop()
 {
-  unsigned long runtime = 0, distance_time = 0, runtime2 = 0;
+  unsigned long runtime1 = 0, distance_time = 0, runtime2 = 0;
   double v_speed;
-  double distance = 154.94; //mm 154.94
+  double distance = 154.94; // mm 154.94
+  long entfernung1, entfernung2;
 
-  runtime = millis();
-  
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Geschwindigkeit:");
-  
-  digitalWrite(trigger1, LOW);
-  delay(5);
-  digitalWrite(trigger1, HIGH);
-  delay(10);
-  digitalWrite(trigger1, LOW);
-  dauer1 = pulseIn(echo1, HIGH);  
-  
-  entfernung1 = (dauer1 / 2) * 0.3432;
-  
 
-  if (entfernung1 < 300){
-    digitalWrite(4, HIGH);
-    do {
-        digitalWrite(trigger2, LOW);
-        delay(5);
-        digitalWrite(trigger2, HIGH);
-        delay(10);
-        digitalWrite(trigger2, LOW);
-        dauer2 = pulseIn(echo2, HIGH);
+  while ((entfernung1 > 100) && (entfernung2 > 100))
+  {
+    entfernung1 = measure_distance(trigger1, echo1);
 
-        entfernung2 = (dauer2 / 2) * 0.3432;
-        runtime2 = millis();
-    } while(entfernung2 > 300);
-    distance_time = runtime2 - runtime;
+    entfernung2 = measure_distance(trigger2, echo2);
+    Serial.print("entfernung1:  ");
+    Serial.println(entfernung1);
+    Serial.print("entfernung2:  ");
+    Serial.println(entfernung2);
+  }
+  Serial.println("START");
+  if (entfernung1 < 100 || entfernung2 < 100)
+  {
+    runtime1 = millis();
+    if (entfernung1 < 100)
+    {
+      Serial.println("E1");
+      digitalWrite(4, HIGH);
+      do
+      {
+        entfernung2 = measure_distance(trigger2, echo2);
+      } while (entfernung2 > 100);
+      runtime2 = millis();
+      digitalWrite(7, HIGH);
+    }
+    else
+    {
+      Serial.println("E2");
+      digitalWrite(7, HIGH);
+      do
+      {
+        entfernung1 = measure_distance(trigger1, echo1);
+      } while (entfernung1 > 100);
+      runtime2 = millis();
+      digitalWrite(4, HIGH);
+    }
+    //}
+    distance_time = runtime2 - runtime1;
     Serial.print(distance_time);
     Serial.print("ms");
-    digitalWrite(7, HIGH);
     // v = s/t
-    v_speed = distance / (double) distance_time;
-    // v_speed *= 3.6;
+    v_speed = distance / (double)distance_time;
 
     Serial.println(v_speed);
-    lcd.setCursor(0,1);
-    lcd.print(v_speed);
-    lcd.print(" m/s");
+    lcd.setCursor(0, 1);
+    lcd.print(v_speed, 2);
+    lcd.print("m/s ");
+    v_speed *= 313.2;
+    lcd.print(v_speed, 0);
+    lcd.print("km/h");
     delay(5000);
   }
-  
   digitalWrite(4, LOW);
   digitalWrite(7, LOW);
-  
+  entfernung1 = 200;
+  entfernung2 = 200;
 }
