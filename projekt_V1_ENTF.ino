3482a68d0306f3609d0b604d8b13ca8ce3a23375
drouver-helmuth <drouver.games@gmail.com>
2022-09-15 12:09:17 +0200

    added comments

--- projekt_V1_ENTF.ino
+++ projekt_V1_ENTF.ino
@@ -6,12 +6,12 @@ long measure_distance(int trigger, int echo)
 {
   long dauer, entfernung;
 
-  digitalWrite(trigger, LOW);
+  digitalWrite(trigger, LOW); // Rücksetzen des Senders 
   delay(5);
-  digitalWrite(trigger, HIGH);
+  digitalWrite(trigger, HIGH); // Aussenden des neuen Signals vom Ultraschallsensor
   delay(2);
   digitalWrite(trigger, LOW);
-  dauer = pulseIn(echo, HIGH);
+  dauer = pulseIn(echo, HIGH);  // Rückgabe in ms
 
   entfernung = (dauer / 2) * 0.3432;
   return entfernung;
@@ -51,61 +51,61 @@ void loop()
 
   do
   {
-    entfernung1 = measure_distance(trigger1, echo1);
+    entfernung1 = measure_distance(trigger1, echo1); // Messen des ersten Sensors
 
-    entfernung2 = measure_distance(trigger2, echo2);
-    Serial.print("entfernung1:  ");
+    entfernung2 = measure_distance(trigger2, echo2); // Messen des zweiten Sensors
+    Serial.print("entfernung1:  "); // Ausgabe der Entfernungen, welche durch Funktion zurückgegeben werden.
     Serial.println(entfernung1);
-    Serial.print("entfernung2:  ");
+    Serial.print("entfernung2:  "); 
     Serial.println(entfernung2);
 
-  } while ((entfernung1 > 100) && (entfernung2 > 100));
+  } while ((entfernung1 > 100) && (entfernung2 > 100)); // Überprüfung ob Objekt Messstrecke befährt oder nicht, wenn ja Aussprung
   Serial.println("START");
-  if (entfernung1 < 100 || entfernung2 < 100)
+  if (entfernung1 < 100 || entfernung2 < 100) // Prüfen ob Sensoren wirklich ausgelöst wurden (2. Ebene)
   {
-    runtime1 = millis();
-    if (entfernung1 < 100)
+    runtime1 = millis(); // Speichern einer Startzeit
+    if (entfernung1 < 100) // Wenn Sensor 1 ausgelöst wurde, starte dieses Programm
     {
       Serial.println("E1");
       lcd.setCursor(13, 1);
-      lcd.print(">>>");
-      digitalWrite(4, HIGH);
+      lcd.print(">>>"); // Ausgabe der Bewegungsrichtung auf dem LCD
+      digitalWrite(4, HIGH);  // Schalte LED ein
       do
       {
-        entfernung2 = measure_distance(trigger2, echo2);
+        entfernung2 = measure_distance(trigger2, echo2); // Messen der zweiten Enfernung, solange die Entfernung größer 100mm ist.
       } while (entfernung2 > 100);
-      runtime2 = millis();
-      digitalWrite(7, HIGH);
+      runtime2 = millis(); // Speichern des Zeitpunktes ab dem der zweite Sensor ausgelöst wurde
+      digitalWrite(7, HIGH); // Schalte LED ein.
     }
-    else
+    else // Starte diese Funktionen, wenn der Sensor 2 ausgelöst wurde
     {
       Serial.println("E2");
       lcd.setCursor(13, 1);
-      lcd.print("<<<");
-      digitalWrite(7, HIGH);
+      lcd.print("<<<"); // Ausgabe der Richtung
+      digitalWrite(7, HIGH); // Einschalten der LED an zuvor ausgelöstem Sensor
       do
       {
         entfernung1 = measure_distance(trigger1, echo1);
-      } while (entfernung1 > 100);
-      runtime2 = millis();
-      digitalWrite(4, HIGH);
+      } while (entfernung1 > 100); // Warte bis zweiter Sensor kleiner ists
+      runtime2 = millis(); // Speichere den Zeitpunkt der zweiten Auslösung
+      digitalWrite(4, HIGH); // Schalte LED 2 ein.
     }
     //}
-    distance_time = runtime2 - runtime1;
+    distance_time = runtime2 - runtime1; // Berechne Zeitdifferenz zwischen Auslösepunkten
     Serial.print(distance_time);
-    Serial.print("ms");
+    Serial.print("ms"); // USB Ausgabe zum Debugging
 
     // v = s/t
-    v_speed = distance / (double)distance_time;
+    v_speed = distance / (double)distance_time; // Berechne Geschwindigkeit
 
     Serial.println(v_speed);
     lcd.setCursor(0, 1);
     lcd.print(v_speed, 2);
-    lcd.print("m/s ");
+    lcd.print("m/s "); // Ausgabe der Geschwindigkeit
     delay(5000);
   }
   digitalWrite(4, LOW);
   digitalWrite(7, LOW);
-  entfernung1 = 200;
+  entfernung1 = 200; // Zurücksetzen der Distanzen
   entfernung2 = 200;
 }
